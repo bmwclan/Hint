f127fed6e14fe9f9be6fcea20b180920f208b53a
bmwclan <36845877+bmwclan@users.noreply.github.com>
2018-02-26 14:45:33 +0200

    Update messages.py

--- messages.py
+++ messages.py
@@ -1,4 +1,4 @@
 messages_list = ["Do not forget to join in on the fun, jump in channel and get starting", 
-"Do not forget to respect the people #No_disrespecting", 
+"Do not forget to respect the people **#No_disrespecting**", 
 "Do not forget to like us on Facebook https://www.facebook.com/BmWPubG/",
 "Welcome to the biggest growing gaming family in the world"]
