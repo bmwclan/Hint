9ac4393e5aba28ecf0a18527c834bd34880632ac
bmwclan <36845877+bmwclan@users.noreply.github.com>
2018-02-26 13:37:08 +0200

    Add bot files

---
+++ Hint.py
@@ -0,0 +1,25 @@
+import os
+import discord
+import asyncio
+import random
+from messages import messages_list
+from copy import deepcopy
+
+key = os.environ['BOT_TOKEN']
+
+client = discord.Client()
+
+async def background_loop():
+    await client.wait_until_ready()
+    await client.change_presence(game=discord.Game(name="Cloak & Dagger"))
+    messages = deepcopy(messages_list)
+    cleanmessages = deepcopy(messages_list)
+    while not client.is_closed:
+        channel = client.get_channel("242956769503084544")
+        if not messages:
+            messages = deepcopy(cleanmessages)
+        await client.send_message(channel, messages.pop(random.randrange(0, len(messages))))
+        await asyncio.sleep(21600)
+
+client.loop.create_task(background_loop())
+client.run(key)
