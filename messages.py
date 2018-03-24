d6bfd3cc25234f66a1b86fec0c5493c3e90a72ee
bmwclan <36845877+bmwclan@users.noreply.github.com>
2018-03-24 11:07:14 +0200

    Update messages.py

--- messages.py
+++ messages.py
@@ -1,7 +1,7 @@
 messages_list = ["Keep in mind to never do anything stupid, or you'll get S H A M E", 
 "If you need help, ask Owner or Admin", 
 "Remember Lightning's rules",
-"Lightning's rule 2. Do not feed her, that is someone else's job"
+"Lightning's rule 2. Do not feed her, that is someone else's job",
 "Lightning's rule 3. Do not scare her",
 "Lightning's rule 4. Do not make Lightning mad, the probability of her attacking you is very high... and there is no stopping her",
 "Do not spam chats",
